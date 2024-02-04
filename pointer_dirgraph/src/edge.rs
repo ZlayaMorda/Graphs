60dd0fbdd8e456fd29e4846988313854876a5eef
ZlayaMorda <tarus01@mail.ru>
2024-02-04 22:16:13 +0100

    add eq for edge and change visibility for crate

--- pointer_dirgraph/src/edge.rs
+++ pointer_dirgraph/src/edge.rs
@@ -4,9 +4,9 @@ use std::hash::Hash;
 
 #[derive(Debug)]
 pub struct Edge<'a, H: Hash + Eq, EdgeData: Display> {
-    data: Option<EdgeData>,
-    node_out: &'a H,
-    node_in: &'a H,
+    pub(crate) data: Option<EdgeData>,
+    pub(crate) node_out: &'a H,
+    pub(crate) node_in: &'a H,
 }
 
 impl <'a, H: Hash + Eq, EdgeData: Display> Edge<'a, H, EdgeData> {
@@ -28,3 +28,11 @@ impl <'a, H: Hash + Eq, EdgeData: Display> fmt::Display for Edge<'a, H, EdgeData
         }
     }
 }
+
+impl <'a, H: Hash + Eq, EdgeData: Display> PartialEq for Edge<'a, H, EdgeData> {
+    fn eq(&self, other: &Self) -> bool {
+        self.node_out == other.node_out && self.node_in == other.node_in
+    }
+}
+
+impl <'a, H: Hash + Eq, EdgeData: Display> Eq for Edge<'a, H, EdgeData> {}
