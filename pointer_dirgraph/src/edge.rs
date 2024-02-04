848a97963f195f03c4324ed33d5b49550d727d73
ZlayaMorda <tarus01@mail.ru>
2024-02-04 19:34:12 +0100

    add basic edge

--- pointer_dirgraph/src/edge.rs
+++ pointer_dirgraph/src/edge.rs
@@ -1,4 +1,30 @@
+use std::fmt;
+use std::fmt::{Display, Formatter};
+use std::hash::Hash;
 
-pub struct Edge {
+#[derive(Debug)]
+pub struct Edge<'a, H: Hash + Eq, EdgeData: Display> {
+    data: Option<EdgeData>,
+    node_out: &'a H,
+    node_in: &'a H,
+}
 
+impl <'a, H: Hash + Eq, EdgeData: Display> Edge<'a, H, EdgeData> {
+
+    pub fn new(data: EdgeData, node_out: &'a H, node_in: &'a H) -> Edge<'a, H, EdgeData> {
+        Edge {
+            data: Some(data),
+            node_out,
+            node_in,
+        }
+    }
+}
+
+impl <'a, H: Hash + Eq, EdgeData: Display> fmt::Display for Edge<'a, H, EdgeData> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match &self.data {
+            Some(data) => {write!(f, "{}", data)}
+            None => {write!(f, "None")}
+        }
+    }
 }
