1e3174cc1446a066fe2013d762791045a225ae07
ZlayaMorda <tarus01@mail.ru>
2024-02-04 19:35:36 +0100

    add traits display, debug, eq and remove index

--- pointer_dirgraph/src/node.rs
+++ pointer_dirgraph/src/node.rs
@@ -1,44 +1,44 @@
 use std::cell::RefCell;
+use std::fmt;
+use std::fmt::{Display, Formatter};
 use std::hash::Hash;
 use std::rc::{Rc, Weak};
 use crate::edge::Edge;
 
-pub struct NodeBuilder <H: Hash, D> {
-    index: H,
-    data: Option<D>,
-    outbound_edges: Vec<Rc<RefCell<Edge>>>,
-    inbound_edges: Vec<Weak<RefCell<Edge>>>,
+#[derive(Default)]
+pub struct NodeBuilder <'a, H: Hash + Eq, NodeData: Display, EdgeData: Display> {
+    data: Option<NodeData>,
+    outbound_edges: Vec<Rc<RefCell<Edge<'a, H, EdgeData>>>>,
+    inbound_edges: Vec<Weak<RefCell<Edge<'a, H, EdgeData>>>>,
 }
 
-impl<H: Hash, D> NodeBuilder<H, D> {
+impl<'a, H: Hash + Eq, NodeData: Display, EdgeData: Display> NodeBuilder<'a, H, NodeData, EdgeData> {
 
-    pub fn new(index: H) -> NodeBuilder<H, D> {
+    pub fn new() -> NodeBuilder<'a, H, NodeData, EdgeData> {
         NodeBuilder {
-            index,
             data: None,
             inbound_edges: vec![],
             outbound_edges: vec![],
         }
     }
 
-    pub fn data(mut self, data: D) -> NodeBuilder<H, D> {
+    pub fn data(mut self, data: NodeData) -> NodeBuilder<'a, H, NodeData, EdgeData> {
         self.data = Some(data);
         self
     }
 
-    pub fn outbound_edges(mut self, outbound_edges: Vec<Rc<RefCell<Edge>>>) -> NodeBuilder<H, D> {
+    pub fn outbound_edges(mut self, outbound_edges: Vec<Rc<RefCell<Edge<'a, H, EdgeData>>>>) -> NodeBuilder<H, NodeData, EdgeData> {
         self.outbound_edges = outbound_edges;
         self
     }
 
-    pub fn inbound_edges(mut self, inbound_edges: Vec<Weak<RefCell<Edge>>>) -> NodeBuilder<H, D> {
+    pub fn inbound_edges(mut self, inbound_edges: Vec<Weak<RefCell<Edge<'a, H, EdgeData>>>>) -> NodeBuilder<H, NodeData, EdgeData> {
         self.inbound_edges = inbound_edges;
         self
     }
 
-    pub fn build(self) -> Node<H, D> {
+    pub fn build(self) -> Node<'a, H, NodeData, EdgeData> {
         Node {
-            index: self.index,
             data: self.data,
             outbound_edges: self.outbound_edges,
             inbound_edges: self.inbound_edges,
@@ -46,9 +46,18 @@ impl<H: Hash, D> NodeBuilder<H, D> {
     }
 }
 
-pub struct Node <H: Hash, D>{
-    index: H,
-    data: Option<D>,
-    outbound_edges: Vec<Rc<RefCell<Edge>>>,
-    inbound_edges: Vec<Weak<RefCell<Edge>>>,
+#[derive(Debug)]
+pub struct Node <'a, H: Hash + Eq, NodeData: Display, EdgeData: Display> {
+    data: Option<NodeData>,
+    outbound_edges: Vec<Rc<RefCell<Edge<'a, H, EdgeData>>>>,
+    inbound_edges: Vec<Weak<RefCell<Edge<'a, H, EdgeData>>>>,
+}
+
+impl<'a, H: Hash + Eq, NodeData: Display, EdgeData: Display> fmt::Display for Node<'a, H, NodeData, EdgeData> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match &self.data {
+            Some(data) => {write!(f, "{}", data)}
+            None => {write!(f, "None")}
+        }
+    }
 }
