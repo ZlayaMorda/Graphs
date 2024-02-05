cde58e7e4f27bba75b92e569b2b5866e37513d37
ZlayaMorda <tarus01@mail.ru>
2024-02-05 10:59:08 +0100

    Change name error, add edge not exist Graph add Display and Clone traits, fix add_edge and add remove_edge

--- pointer_dirgraph/src/errors.rs
+++ pointer_dirgraph/src/errors.rs
@@ -1,11 +1,13 @@
 use thiserror::Error;
 
 #[derive(Debug, Error)]
-pub enum AppError {
+pub enum GraphError {
     #[error("Node with the current index already exists")]
     OccupiedError(),
     #[error("Node with the current index does not exist")]
     NodeNotExist(),
     #[error("Same edge already exists")]
     EdgeExist(),
+    #[error("Edge between such nodes does not exist")]
+    EdgeNotExist(),
 }
--- pointer_dirgraph/src/graph.rs
+++ pointer_dirgraph/src/graph.rs
@@ -4,18 +4,18 @@ use std::fmt::Display;
 use std::hash::Hash;
 use std::rc::{Rc, Weak};
 use crate::edge::Edge;
-use crate::errors::AppError;
-use crate::errors::AppError::{EdgeExist, NodeNotExist, OccupiedError};
+use crate::errors::GraphError;
+use crate::errors::GraphError::{EdgeExist, EdgeNotExist, NodeNotExist, OccupiedError};
 use crate::node::Node;
 
 #[derive(Default, Debug)]
-pub struct Graph<'a, H: Hash + Eq, NodeData: Display, EdgeData: Display> {
+pub struct Graph<'a, H: Hash + Eq + Display, NodeData: Display + Clone, EdgeData: Display + Clone> {
     nodes: HashMap<H, Node<'a, H, NodeData, EdgeData>>
 }
 
-impl <'a, H: Hash + Eq, NodeData: Display, EdgeData: Display> Graph<'a, H, NodeData, EdgeData> {
+impl <'a, H: Hash + Eq + Display, NodeData: Display + Clone, EdgeData: Display + Clone> Graph<'a, H, NodeData, EdgeData> {
 
-    pub fn add_node(&mut self, key: H, node: Node<'a, H, NodeData, EdgeData>) -> Result<(), AppError> {
+    pub fn add_node(&mut self, key: H, node: Node<'a, H, NodeData, EdgeData>) -> Result<(), GraphError> {
         if self.nodes.get(&key).is_some(){
             Err(OccupiedError())
         } else {
@@ -30,35 +30,54 @@ impl <'a, H: Hash + Eq, NodeData: Display, EdgeData: Display> Graph<'a, H, NodeD
         self.nodes.get(&key)
     }
 
-    pub fn add_edge(&mut self, edge_data: EdgeData, outbound: &'a H, inbound: &'a H) -> Result<(), AppError> {
+    pub fn add_edge(&mut self, edge_data: EdgeData, outbound: &'a H, inbound: &'a H) -> Result<(), GraphError> {
         let new_edge : Edge<H, EdgeData> = Edge::new(edge_data, outbound, inbound);
-        match self.not_contain_edge_and_nodes(&new_edge) {
+        match self.not_contain_edge_and_nodes(new_edge.node_out, new_edge.node_in) {
             Ok(_) => {
                 let new_edge : Rc<RefCell<Edge<H, EdgeData>>> = Rc::new(RefCell::new(new_edge));
                 let weak_edge : Weak<RefCell<Edge<H, EdgeData>>> = Rc::downgrade(&new_edge);
-
-                let outbound: &mut Node<H, NodeData, EdgeData> = self.nodes.get_mut(outbound)
+                let outbound_node: &mut Node<H, NodeData, EdgeData> = self.nodes.get_mut(outbound)
                     .expect("node does not exist"); // checked if node exists
-                outbound.outbound_edges.push(new_edge);
+                outbound_node.outbound_edges.insert(outbound, new_edge);
 
-                let inbound: &mut Node<H, NodeData, EdgeData> = self.nodes.get_mut(inbound)
+                let inbound_node: &mut Node<H, NodeData, EdgeData> = self.nodes.get_mut(inbound)
                     .expect("node does not exist");
-                inbound.inbound_edges.push(weak_edge);
+                inbound_node.inbound_edges.insert(inbound, weak_edge);
                 Ok(())
             }
             Err(error) => { Err(error) }
         }
     }
 
-    pub fn not_contain_edge_and_nodes(&self, new_edge: &Edge<H, EdgeData>) -> Result<(), AppError> {
-        match self.nodes.get(new_edge.node_out) {
-            Some(outbound_node) => {
-                match self.nodes.get(new_edge.node_in) {
+    pub fn remove_edge(&mut self, outbound: &'a H, inbound: &'a H) -> Result<(), GraphError> {
+        match self.nodes.get_mut(outbound) {
+            Some(node) => {
+                match node.outbound_edges.remove(&outbound) {
                     Some(_) => {
-                        for edge in &outbound_node.outbound_edges {
-                            if edge.borrow().eq(new_edge) {
-                                return Err(EdgeExist())
+                        match self.nodes.get_mut(inbound) {
+                            Some(in_node) => {
+                                match in_node.inbound_edges.remove(&inbound) {
+                                    Some(_) => { Ok(()) }
+                                    None => Err(NodeNotExist())
+                                }
                             }
+                            None => { Err(NodeNotExist())}
+                        }
+                    }
+                    None => { Err(EdgeNotExist()) }
+                }
+            }
+            None => { Err(NodeNotExist()) }
+        }
+    }
+
+    pub fn not_contain_edge_and_nodes(&self, outbound: &H, inbound: &H) -> Result<(), GraphError> {
+        match self.nodes.get(outbound) {
+            Some(outbound_node) => {
+                match self.nodes.get(inbound) {
+                    Some(_) => {
+                        if outbound_node.outbound_edges.contains_key(outbound) {
+                            return Err(EdgeExist())
                         }
                         Ok(())
                     },
