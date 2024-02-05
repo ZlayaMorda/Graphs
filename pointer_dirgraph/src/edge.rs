0694e936abb83fee9b599d6ed05899572c3da105
ZlayaMorda <tarus01@mail.ru>
2024-02-05 10:57:40 +0100

    Edge add Display and Clone traits

--- pointer_dirgraph/src/edge.rs
+++ pointer_dirgraph/src/edge.rs
@@ -3,13 +3,13 @@ use std::fmt::{Display, Formatter};
 use std::hash::Hash;
 
 #[derive(Debug)]
-pub struct Edge<'a, H: Hash + Eq, EdgeData: Display> {
+pub struct Edge<'a, H: Hash + Eq + Display, EdgeData: Display + Clone> {
     pub(crate) data: Option<EdgeData>,
     pub(crate) node_out: &'a H,
     pub(crate) node_in: &'a H,
 }
 
-impl <'a, H: Hash + Eq, EdgeData: Display> Edge<'a, H, EdgeData> {
+impl <'a, H: Hash + Eq + Display, EdgeData: Display + Clone> Edge<'a, H, EdgeData> {
 
     pub fn new(data: EdgeData, node_out: &'a H, node_in: &'a H) -> Edge<'a, H, EdgeData> {
         Edge {
@@ -20,7 +20,7 @@ impl <'a, H: Hash + Eq, EdgeData: Display> Edge<'a, H, EdgeData> {
     }
 }
 
-impl <'a, H: Hash + Eq, EdgeData: Display> fmt::Display for Edge<'a, H, EdgeData> {
+impl <'a, H: Hash + Eq + Display, EdgeData: Display + Clone> fmt::Display for Edge<'a, H, EdgeData> {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match &self.data {
             Some(data) => {write!(f, "{}", data)}
@@ -29,10 +29,10 @@ impl <'a, H: Hash + Eq, EdgeData: Display> fmt::Display for Edge<'a, H, EdgeData
     }
 }
 
-impl <'a, H: Hash + Eq, EdgeData: Display> PartialEq for Edge<'a, H, EdgeData> {
+impl <'a, H: Hash + Eq + Display, EdgeData: Display + Clone> PartialEq for Edge<'a, H, EdgeData> {
     fn eq(&self, other: &Self) -> bool {
         self.node_out == other.node_out && self.node_in == other.node_in
     }
 }
 
-impl <'a, H: Hash + Eq, EdgeData: Display> Eq for Edge<'a, H, EdgeData> {}
+impl <'a, H: Hash + Eq + Display, EdgeData: Display + Clone> Eq for Edge<'a, H, EdgeData> {}
