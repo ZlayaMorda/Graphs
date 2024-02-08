a1fcc660ff6730229012285e93bc15f61b5f85ab
ZlayaMorda <tarus01@mail.ru>
2024-02-08 05:57:38 +0100

    main graph dft deserialization and dfs

--- usage/src/main.rs
+++ usage/src/main.rs
@@ -1,5 +1,10 @@
-use pointer_dirgraph::add;
+use pointer_dirgraph::dft::deserializer::DftDeserializer;
+use pointer_dirgraph::graph::Graph;
 
 fn main() {
-    println!("init {}", add(1, 2));
+    let mut deserialized_graph: Graph<u32, u32, String> = Graph::default();
+    if let Err(e) = deserialized_graph.deserialize("graph.txt") {
+        println!("Error while deserialize graph: {e}")
+    }
+    deserialized_graph.dfs(&1);
 }
